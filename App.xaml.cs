a38e3bd10902cbdef910e58a089a38020ec61223
Hamza Driouch <hdri@concepment.com>
2025-04-16 13:01:10 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorNotify
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
